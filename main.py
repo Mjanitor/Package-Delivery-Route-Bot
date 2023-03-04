188bc190f2f3a7c4c1fcecd81cf2fc12859e943a
Mjanitor <mikejanitor@gmail.com>
2023-03-04 12:47:16 -0500

    Initializing Truck Attributes and creating a test truck

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from truck import *
+
 def main():
     print("Hello World!")
     print("Doggies are fun")
@@ -6,6 +8,9 @@ def main():
 
     for x in range(5):
         print(x)
+
+    truck1 = Truck("Truck 1", 2)
+    print(truck1)
 
 if __name__ == "__main__":
     main()
--- truck.py
+++ truck.py
@@ -1,3 +1,7 @@
 class Truck:
-    def __init__(self):
-        pass
+    def __init__(self, name, packages):
+        self.name = name
+        self.packages = packages
+
+    def __str__(self):
+        return f"This truck has {self.packages} packages."
