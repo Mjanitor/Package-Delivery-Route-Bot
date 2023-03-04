81e65809b0f000508fca781920585862dc9a9709
Mjanitor <mikejanitor@gmail.com>
2023-03-04 12:59:35 -0500

    Initializing Package Attributes and creating a test package

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from truck import *
+from package import *
 
 def main():
     print("Hello World!")
@@ -9,8 +10,13 @@ def main():
     for x in range(5):
         print(x)
 
+    # Demo Truck
     truck1 = Truck("Truck 1", 2)
     print(truck1)
+
+    # Demo Package
+    package1 = Package(1, "195 W Oakland Ave", "Salt Lake City", "UT", 84115, 1030, 21)
+    print(package1)
 
 if __name__ == "__main__":
     main()
--- package.py
+++ package.py
@@ -1,3 +1,13 @@
 class Package:
-    def __init__(self):
-        pass
+    def __init__(self, id, address, city, state, zip, deadline, weight):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deadline = deadline
+        self.weight = weight
+
+    def __str__(self):
+        return f"This package has an ID of {self.id}.  It is being delivered to {self.address}, {self.city}, {self.state} {self.zip}.  It weighs {self.weight} Kilos " \
+               f"and must be delivered by {self.deadline}."
