badd08c71db8a4932810228e333efdd50c529ca8
Mjanitor <mikejanitor@gmail.com>
2023-03-05 13:42:04 -0500

    Adjusting package layout

--- package.py
+++ package.py
@@ -1,13 +1,13 @@
 class Package:
-    def __init__(self, id, address, city, state, zip, deadline, weight):
+    def __init__(self, id, address, deadline, city, zip, weight, status):
         self.id = id
         self.address = address
-        self.city = city
-        self.state = state
-        self.zip = zip
         self.deadline = deadline
+        self.city = city
+        self.zip = zip
         self.weight = weight
+        self.status = status
 
     def __str__(self):
-        return f"This package has an ID of {self.id}.  It is being delivered to {self.address}, {self.city}, {self.state} {self.zip}.  It weighs {self.weight} Kilos " \
+        return f"This package has an ID of {self.id} and is currently {self.status}.  It is being delivered to {self.address}, {self.city} {self.zip}.  It weighs {self.weight} Kilos " \
                f"and must be delivered by {self.deadline}."
