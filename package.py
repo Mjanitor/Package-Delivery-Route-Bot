3f78138dc714f3d09a66afd4a506b708de73eddf
Mjanitor <mikejanitor@gmail.com>
2023-03-05 14:50:50 -0500

    Begrudgingly re-adding the 'State' attribute despite everything being in Utah

--- package.py
+++ package.py
@@ -1,15 +1,16 @@
 class Package:
-    def __init__(self, id, address, deadline, city, zip, weight, status):
+    def __init__(self, id, address, deadline, city, state, zip, weight, status):
         self.id = id
         self.address = address
         self.deadline = deadline
         self.city = city
+        self.state = state
         self.zip = zip
         self.weight = weight
         self.status = status
 
     def __str__(self):
-        return f"This package has an ID of {self.id} and is currently {self.status}.  It is being delivered to {self.address}, {self.city} {self.zip}.  It weighs {self.weight} Kilos " \
+        return f"This package has an ID of {self.id} and is currently {self.status}.  It is being delivered to {self.address}, {self.city}, {self.state} {self.zip}.  It weighs {self.weight} Kilos " \
                f"and must be delivered by {self.deadline}."
     def check_location(self):
         print(f"Package {self.id} is currently {self.status}")
