84f58492230d1bb082b3a5043f7327c521e55a55
Mjanitor <mikejanitor@gmail.com>
2023-03-05 13:44:28 -0500

    Amending main package layout

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     print(truck1)
 
     # Demo Package
-    package1 = Package(1, "195 W Oakland Ave", "Salt Lake City", "UT", 84115, 1030, 21)
+    package1 = Package(1, "195 W Oakland Ave", 1030, "Salt Lake City", 84115, 21, "at the Hub")
     print(package1)
 
     # Demo Hashmap
