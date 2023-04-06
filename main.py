25b3590a2a295c6535cd84c1cb70a423c661bfbf
Mjanitor <mikejanitor@gmail.com>
2023-04-06 16:35:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,179 +1,240 @@
-# Identifying comment: Michael Janitor, ID # 010769292
+# Written by Mike Janitor, Student ID: 010769292
 
-from truck import *
-from package import *
-from hashmap import *
+import Truck
+from Package import *
+import HashMap
 import csv
-import datetime
+from datetime import *
 
-# Importing Package CSV data
-with open("Resources/WGUPS Package File.csv", encoding='utf-8-sig') as csvfile:
-    packageCSV = csv.reader(csvfile)
-    packageList = list(packageCSV)
+# Loads a csv file as a parameter and opens that file in a list format
+def openCsvAsList(filepath):
+    with open(filepath, encoding="utf-8-sig") as file:
+        csv_list = []
+        reader = csv.reader(file)
+        for row in reader:
+            csv_list.append(row)
+        return csv_list
 
-# Importing Distance CSV data
-with open("Resources/WGUPS Distance Table.csv", encoding='utf-8-sig') as csvfile:
-    distanceCSV = csv.reader(csvfile)
-    distanceList = list(distanceCSV)
+# Generates all packages for a given truck and inserts them into the given hashmap
+def createPackages(hashMap, package_item_list, truck1, truck2):
+    for package in package_item_list:
 
-# Importing Address CSV data
-with open("Resources/Address Indices.csv", encoding='utf-8-sig') as csvfile:
-    addressCSV = csv.reader(csvfile)
-    addressList = list(addressCSV)
-
-
-# Reading CSV files to create packages with data, then adding them to the Hash Map
-def load_packages(file_name, hash_table):
-    # Adding package data based on columns in the CSV List
-    for package in file_name:
-        ID = package[0]
+        id = package[0]
         address = package[1]
         city = package[2]
-        state = package[3]
         zip = package[4]
-        deadline = package[5]
+        time = package[5]
         weight = package[6]
-        status = "at the Hub"
 
-        # Creating a new package object
-        package = Package(ID, address, city, state, zip, deadline, weight, status)
+        # Manually adding package departure times depending on which truck it's loaded into
+        if int(id) in truck1.packages:
+            departure_time = timedelta(hours=8, minutes=0, seconds=0)
+        elif int(id) in truck2.packages:
+            departure_time = timedelta(hours=10, minutes=20, seconds=0)
+        else:
+            departure_time = timedelta(hours=9, minutes=5, seconds=0)
 
-        # Adding ethe new package
-        hash_table.add(ID, package)
+        arrival_time = timedelta(hours=0, minutes=0, seconds=0)
 
+        # Constructor
+        new_package = Package(id, address, city, zip, time, weight, departure_time, arrival_time)
 
-# Turning addresses into indices for distance calculation
-def get_address_index(address):
-    for entry in addressList:
-        if entry[2] == address:
-            return int(entry[0])
+        # Insertion
+        hashMap.insert(id, new_package)
 
+# Finds the distance (in miles) between two given addresses
+def find_distance(address1, address2, address_list, distance_list):
+    address1_index = 0
+    address2_index = 0
 
-# Create Method to determine the distance between two addresses
-def find_distance(x, y):
-    distance = distanceList[x][y]
+    # Parsing address list and capturing the associated index if found
+    for item in address_list:
+        if item[2] == address1:
+            address1_index = int(item[0])
+        if item[2] == address2:
+            address2_index = int(item[0])
 
-    # Flips the lookup index in case of empty distance value
-    if distance == '':
-        distance = distanceList[y][x]
+    # Taking the indexes and matching them up in the distance list
+    if distance_list[address1_index][address2_index] != '':
+        distance = distance_list[address1_index][address2_index]
+    elif distance_list[address2_index][address1_index] != '':
+        distance = distance_list[address2_index][address1_index]
+    else:
+        distance = None
 
     return distance
 
+def nearest_neighbor(truck, hash_table, address_list, distance_list, package_list):
+    # Counter
+    mileage = 0
 
-# Creating hash map
-hashMap = HashMap()
+    # Main nearest-neighbor loop
+    while len(truck.packages) > 0:
+        closest_distance = 1000.0
+        for package in truck.packages:
+            current_package = package  # temporary holder
+            package = package_list[current_package - 1][0]
+            package_object = hash_table.lookup(str(package))
+            package_address = package_object.delivery_address
+            temp_distance = float(find_distance(truck.location, package_address, address_list, distance_list))
 
-# Creating Truck Objects
-truck1 = Truck("Truck 1", 16, 18, [1, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40], 0.0, "4001 South 700 East",
-               datetime.timedelta(hours=8))
-truck2 = Truck("Truck 2", 16, 18, [3, 12, 17, 18, 21, 22, 23, 24, 26, 27, 35, 36, 38, 39], 0.0, "4001 South 700 East",
-               datetime.timedelta(hours=10, minutes=20))
-truck3 = Truck("Truck 3", 16, 18, [2, 4, 5, 6, 7, 8, 9, 10, 11, 25, 28, 32, 33], 0.0, "4001 South 700 East",
-               datetime.timedelta(hours=9, minutes=5))
+            # Cuts off early if 0 mile distance is found
+            if temp_distance == 0:
+                final_package = current_package
+                final_package_object = package_object
+                final_address = package_address
+                closest_distance = temp_distance
+                break
+            # If closer than the previous item, updates variables with current package attributes
+            elif temp_distance < closest_distance:
+                final_package = current_package
+                final_package_object = package_object
+                final_address = package_address
+                closest_distance = temp_distance
 
-load_packages(packageList, hashMap)
+        # Updating time based on 18mph speed
+        travel_time = closest_distance / 18
+        truck.time += timedelta(hours=travel_time)
+        final_package_object.arrival_time = truck.time
 
+        # Updating mileage with the captured distance
+        mileage += closest_distance
+        truck.location = final_address  # Updating the truck's location to the delivery address
+        truck.packages.remove(final_package)  # Removing the package from the list to deliver
 
-# Main package delivery
-def package_delivery(truck):
-    # Nearest Neighbor Algorithm
-    mileage = truck.mileage
-    undelivered = []
+    return mileage
 
-    # Adding all truck's loaded packages to an iterable list of undelivered items
-    for package in truck.packages:
-        undelivered.append(package)
-        hashMap.get(str(package)).departure_time = truck.time  # Capturing the packages' departure time to match truck's
+# Main Loop
+# Generates all necessary truck objects, creates the Hash Table, loads them with packages,
+# generates the total mileage
+def main():
+    total_mileage = 0
+    # Manually loading the trucks
+    # 8:00am
+    truck1 = Truck.Truck("Truck 1", 0, [2, 4, 5, 7, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34], "4001 South 700 East",
+                         timedelta(hours=8, minutes=0, seconds=0))
+    # Wrong Address, 10:20am
+    truck2 = Truck.Truck("Truck 2", 0, [3, 8, 9, 10, 11, 12, 17, 18, 21, 22, 23, 36, 38], "4001 South 700 East",
+                         timedelta(hours=10, minutes=20, seconds=0))
+    # 9:05am
+    truck3 = Truck.Truck("Truck 3", 0, [1, 6, 24, 25, 26, 27, 28, 32, 33, 35, 37, 39, 40], "4001 South 700 East",
+                         timedelta(hours=9, minutes=5, seconds=0))
 
-    # Capturing addresses and distance between truck and initial package
-    current_package = undelivered[0]
-    cur_package_address = addressList[get_address_index(hashMap.get(str(current_package)).address)][2]
-    final_package_address = None
-    distance = float(find_distance(get_address_index(truck.address), get_address_index(cur_package_address)))
+    # Loading CSV data
+    address_list = openCsvAsList("Resources\Address Indices.csv")
+    package_list = openCsvAsList("Resources\WGUPS Package File.csv")
+    distance_list = openCsvAsList("Resources\WGUPS Distance Table.csv")
 
-    # Initial delivery (Fencepost)
-    truck.time += datetime.timedelta(hours=distance / 18)
-    hashMap.get(str(current_package)).delivery_time = truck.time
-    mileage += distance
-    truck.address = cur_package_address
-    undelivered.pop(undelivered.index(current_package))  # Removing it from the undelivered list for loop iteration
+    # Creating Hash Table
+    hash_table = HashMap.HashMap()
 
-    # Find the shortest package distance
-    while len(undelivered) > 0:
-        for package in undelivered:
-            cur_package_address = addressList[get_address_index(hashMap.get(str(package)).address)][2]
-            temp_distance = float(
-                find_distance(get_address_index(truck.address), get_address_index(cur_package_address)))
-            if temp_distance < distance:
-                distance = temp_distance
-                current_package = package
-                final_package_address = cur_package_address
+    # Generating packages for each Truck
+    createPackages(hash_table, package_list, truck1, truck2)
 
-        # Updating tracking information
-        truck.time += datetime.timedelta(hours=distance / 18)
-        hashMap.get(str(current_package)).delivery_time = truck.time
-        mileage += float(distance)
-        truck.address = final_package_address
-        undelivered.pop(undelivered.index(current_package))
-        # Resetting values for loop
-        distance = 1000
-        current_package = None
+    # Algorithm x 3 = mileage
+    # Adding Truck 1 mileage
+    total_mileage += nearest_neighbor(truck1, hash_table, address_list, distance_list, package_list)
+    # Driving Truck 1 home so driver can drive Truck 3
+    total_mileage += float(find_distance(truck1.location, "4001 South 700 East", address_list, distance_list))
+    # Adding Truck 2 mileage
+    total_mileage += nearest_neighbor(truck2, hash_table, address_list, distance_list, package_list)
+    # Adding Truck 3 mileage
+    total_mileage += nearest_neighbor(truck3, hash_table, address_list, distance_list, package_list)
 
-    # Updating from the working 'mileage' for all deliveries
-    truck.mileage = mileage
-
-    # Accounting for first truck's return home
-    if truck == truck1:
-        truck.mileage += float(
-            find_distance(get_address_index(truck.address), get_address_index("4001 South 700 East")))
-
-    # Rounding after all un-rounded values have been totaled
-    return round(truck.mileage, 2)
-
-
-# Three trips
-first_trip = package_delivery(truck1)
-second_trip = package_delivery(truck2)
-third_trip = package_delivery(truck3)
-
-
-# Interface implementation
-class Main:
-    # Main loop and prompt
+    # Interface loop
     while True:
-        print("------------------------------------------------------")
-        print("Welcome to the WGUPS Status Interface!")
-        print("Here, you can check the status of any package at any time")
-        time = ""
-        # Validating input
-        while True:
-            time = input("Enter a time after 8AM (format: 0930) to check the status of a package. \n Type 'X' to exit: ")
-            if time.lower() == "x":
-                exit()
-            elif time.isnumeric() == False or len(time) != 4:
-                print("Please check your formatting and try again.")
-                continue
-            else:
-                h = time[:2]  # First two characters
-                m = time[2:]  # Second two characters
-                lookup_time = datetime.timedelta(hours=int(h), minutes=int(m))
-                selection = input("Would you like all packages or just one?  If one, type 'solo': ")
-                # Individual lookup
-                if selection == "solo":
-                    package_ID = int(input("Packages range from 1 - 40.  Please enter the Package ID you would like to search: "))
-                    while package_ID < 1 or package_ID > 40:
-                        package_ID = int(input("Invalid Package number.  Please enter a number between 1 and 40: "))
-                    lookup_package = hashMap.get(str(package_ID))
-                    lookup_package.update_delivery_status(lookup_time)
-                    print("------------------------------------------------------")
-                    print(f"Status at: {lookup_time}\nPackage ID: {package_ID}, Status: [{lookup_package.status}], Departure Time: {lookup_package.departure_time}, Delivery Time: {lookup_package.delivery_time} \n")
-                # All Packages lookup
+        # Making it pretty
+        print("Welcome to WGUPS package locator!")
+        input_time = input("Please enter a time to check package status (format - 0930) - (Type 'X' to exit): ")
+
+        # Quit condition
+        if input_time.lower() == "X".lower():
+            break
+
+        # Looping for correct formatting
+        while len(input_time) != 4 or not input_time.isnumeric():
+            print("Wrong format, please try again.")
+            input_time = input("Please enter a time to check package status (format - 0930): ")
+
+        # Slicing the input time for timedelta format
+        delivery_hour = int(input_time[:2])
+        delivery_min = int(input_time[2:])
+        input_time = timedelta(hours=delivery_hour, minutes=delivery_min)
+
+        # Solo or all?
+        amount = input("Would you like to check the status of all packages or just one? "
+                       "Type 'solo' or 'all': ")
+
+        # Printing all packages
+        if amount.lower() == "all".lower():
+            print(f"Lookup Time: {input_time}")
+            print("-------------------------------------------------------------------------------------"
+                  "----------------------------")
+            print(f"ID || Delivery Address || Delivery City ||  ZIP || "
+                  f"Weight || Departure Time || Delivery Time || Deadline || Status")
+            print("-------------------------------------------------------------------------------------"
+                  "----------------------------")
+            for package in range(1, 41):
+                # Printing status
+                package = hash_table.lookup(str(package))
+                package_status = Package.getStatus(package, input_time)
+
+                # Changing colors based on delivery status
+                if package_status == "Delivered":
+                    print(f"\033[0;32m {package.id} || {package.delivery_address} || {package.delivery_city} || {package.delivery_zip} || "
+                          f"{package.weight} kilograms || {package.departure_time} || {package.arrival_time} || {package.delivery_deadline} || {package_status}")
+                elif package_status == "En Route":
+                    print(f"\033[1;33m {package.id} || {package.delivery_address} || {package.delivery_city} || {package.delivery_zip} || "
+                          f"{package.weight} kilograms || {package.departure_time} || {package.arrival_time} || {package.delivery_deadline} || {package_status}")
                 else:
-                    # Implement all package statuses
-                    for package in range(1, 41):
-                        package = hashMap.get(str(package))
-                        package.update_delivery_status(lookup_time)
-                        print("------------------------------------------------------")
-                        print(f"Status at: {lookup_time}\nPackage ID: {package.id}, Status: [{package.status}], Departure Time: {package.departure_time}, Delivery Time: {package.delivery_time} \n")
-                    print("------------------------------------------------------\n")
-                    print(f"Total Miles: {first_trip + second_trip + third_trip} \n")
+                    print(f"\033[0;31m {package.id} || {package.delivery_address} || {package.delivery_city} || {package.delivery_zip} || "
+                          f"{package.weight} kilograms || {package.departure_time} || {package.arrival_time} || {package.delivery_deadline} || {package_status}")
+
+            # Total miles
+            print("\033[0m-------------------------------------------------------------------------------------"
+                  "----------------------------")
+            print(f"Total miles driven: {total_mileage} miles")
+            print("\n")
+
+        # Printing solo package
+        elif amount.lower() == "solo".lower():
+            package_id = input("Please enter a package ID: ")
+
+            print(f"Lookup Time: {input_time}")
+            print("-------------------------------------------------------------------------------------"
+                  "----------------------------")
+            print(f"ID || Delivery Address || Delivery City ||  ZIP || "
+                  f"Weight || Departure Time || Delivery Time || Deadline || Status")
+            print("-------------------------------------------------------------------------------------"
+                  "----------------------------")
+
+            # Printing status
+            package = hash_table.lookup(str(package_id))
+            package_status = Package.getStatus(hash_table.lookup(package_id), input_time)
+
+            # Changing colors based on delivery status
+            if package_status == "Delivered":
+                print(
+                    f"\033[0;32m {package.id} || {package.delivery_address} || {package.delivery_city} || {package.delivery_zip} || "
+                    f"{package.weight} kilograms || {package.departure_time} || {package.arrival_time} || {package.delivery_deadline} || {package_status}")
+            elif package_status == "En Route":
+                print(
+                    f"\033[1;33m {package.id} || {package.delivery_address} || {package.delivery_city} || {package.delivery_zip} || "
+                    f"{package.weight} kilograms || {package.departure_time} || {package.arrival_time} || {package.delivery_deadline} || {package_status}")
+            else:
+                print(
+                    f"\033[0;31m {package.id} || {package.delivery_address} || {package.delivery_city} || {package.delivery_zip} || "
+                    f"{package.weight} kilograms || {package.departure_time} || {package.arrival_time} || {package.delivery_deadline} || {package_status}")
+
+            # Total miles
+            print("\033[0m-------------------------------------------------------------------------------------"
+                  "----------------------------")
+            print(f"Total miles driven: {total_mileage} miles")
+            print("\n")
+
+        # Formatting not recognized
+        else:
+            print("Sorry, I didn't understand that, please try again.\n")
+
+if __name__ == '__main__':
+    main()
